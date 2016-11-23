6f3e8053cb566a159d8e44668a879302a88ebbb1
enonz <aj.biner@gmail.com>
2016-11-23 17:19:57 +0700

    ini seed untuk data dummynya

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 User.create!([
-  {email: "aj.biner@gmail.com", encrypted_password: "$2a$11$LjKlstc9n3IeYDhNBQ9QWebYJyZsYMQbQOurtvzIWXiDXzDGQJOoK", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 3, current_sign_in_at: "2016-11-23 07:30:11", last_sign_in_at: "2016-11-23 06:38:00", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", admin: false},
-  {email: "ainun.jariya@emveep.com", encrypted_password: "$2a$11$xo0h9ezaUYhw.NVwedZilucbQykNwWC6KVtSBA4J8Mx480y4sC0dy", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 3, current_sign_in_at: "2016-11-23 06:11:33", last_sign_in_at: "2016-11-23 05:04:49", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", admin: true}
+  {email: "aj.biner@gmail.com", encrypted_password: "$2a$11$LjKlstc9n3IeYDhNBQ9QWebYJyZsYMQbQOurtvzIWXiDXzDGQJOoK", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 4, current_sign_in_at: "2016-11-23 09:44:14", last_sign_in_at: "2016-11-23 07:30:11", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", admin: false},
+  {email: "ainun.jariya@emveep.com", encrypted_password: "$2a$11$xo0h9ezaUYhw.NVwedZilucbQykNwWC6KVtSBA4J8Mx480y4sC0dy", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 5, current_sign_in_at: "2016-11-23 10:16:37", last_sign_in_at: "2016-11-23 09:59:49", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", admin: true}
 ])
 Article.create!([
   {title: "Cerita Pagi", content: "hawa yang diam demikian indah menghadirkan bola matahari dengan kemuning sinarnya saya teringat kamu terpuruk di sudut kamar menyanyi tanpa kata-kata rinduku menyulam pintalan doa tanpa letih. . . untuk pagi mu yang indah selamat pagi. . ", image: ""},
@@ -8,5 +8,6 @@ Article.create!([
 ])
 Comment.create!([
   {content: "tentang bulan .. about moon (y)", user_id: 2, article_id: 2},
-  {content: "tentang bulan .. about moon (y)", user_id: 2, article_id: 2}
+  {content: "tentang bulan .. about moon (y)", user_id: 2, article_id: 2},
+  {content: "seperti kenyataan yang sejati, pagi adalah kemurnian hati ~~~", user_id: 2, article_id: 1}
 ])
